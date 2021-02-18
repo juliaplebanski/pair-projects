fce7d8ffef48d312f1c5afedccf055cab2282de2
Julia Plebanski <julplebanski@gmail.com>
2021-02-18 17:51:52 -0500

    capstone started

--- 09_Capstone/src/main/java/com/techelevator/model/JDBC/JDBCReservationDAO.java
+++ 09_Capstone/src/main/java/com/techelevator/model/JDBC/JDBCReservationDAO.java
@@ -1,5 +1,71 @@
 package com.techelevator.model.JDBC;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.dao.ReservationDAO;
+
+import com.techelevator.model.domain.Reservation;
+
 public class JDBCReservationDAO {
 
+	public class JDBCReservationAO implements ReservationDAO {
+
+		private JdbcTemplate jdbcTemplate;
+
+		public void JDBCReservationDAO(DataSource dataSource) {
+			this.jdbcTemplate = new JdbcTemplate(dataSource);
+		}
+
+		@Override
+		public List<Reservation> getAllReservations() {
+
+			List<Reservation> reservationList = new ArrayList<Reservation>();
+			String selectSQL = "SELECT * FROM reservation";
+			SqlRowSet results = jdbcTemplate.queryForRowSet(selectSQL);
+			while (results.next()) {
+				Reservation reservation = mapRowToReservation(results);
+				reservationList.add(reservation);
+
+			}
+			return reservationList;
+
+		}
+
+		@Override
+		public List<Reservation> getAvailiableReservations(LocalDate startDate, LocalDate endDate, long spaceID) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void createReservation(Reservation reservation) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public Reservation getReservation(long reservationID) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+	private Reservation mapRowToReservation(SqlRowSet results) {
+		Reservation reservation = new Reservation();
+		// local date not working? reservation.setEndDate(results.getLocalDate);
+		reservation.setName(results.getString("name"));
+		reservation.setReservationID(results.getLong("reservation_id"));
+		reservation.setSpaceID(results.getLong("space_id"));
+		// local date not working? reservation.setStartDate(results.getLocalDate));
+
+		return reservation;
+	}
 }
--- 09_Capstone/src/main/java/com/techelevator/model/JDBC/JDBCVenueDAO.java
+++ 09_Capstone/src/main/java/com/techelevator/model/JDBC/JDBCVenueDAO.java
@@ -1,5 +1,63 @@
 package com.techelevator.model.JDBC;
 
-public class JDBCVenueDAO {
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import com.techelevator.model.dao.VenueDAO;
+import com.techelevator.model.domain.Venue;
+
+public class JDBCVenueDAO implements VenueDAO {
+
+	private JdbcTemplate jdbcTemplate;
+
+	public JDBCVenueDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@Override
+	public List<Venue> getAllVenues() {
+		List<Venue> venueList = new ArrayList<Venue>();
+		String selectSQL = "SELECT venue.name FROM venue ORDER BY venue.name";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(selectSQL);
+		while (results.next()) {
+			Venue venue = mapRowToVenue(results);
+			venueList.add(venue);
+
+		}
+		return venueList;
+	}
+
+	public List<Venue> getVenueDetails() {
+		
+		List<Venue> venueDetails = new ArrayList<Venue>();
+		String selectSQL = "SELECT venue.name, (city.name, city.state_abbreviation) AS location_id, (category.name) AS categories FROM venue\n" + 
+				"JOIN category_venue ON category_venue.venue_id = venue.id\n" + 
+				"JOIN category ON category_venue.category_id = category.id\n" + 
+				"JOIN city ON venue.city_id = city.id\n" + 
+				"WHERE venue.id = 5";
+		//how to print multiple categories a venue has on the same line instead of two separate
+		SqlRowSet results = jdbcTemplate.queryForRowSet(selectSQL);
+		while (results.next()) {
+			Venue venue = mapRowToVenue(results);
+			venueDetails.add(venue);
+		}
+		return venueDetails;
+	}
+
+	private Venue mapRowToVenue(SqlRowSet results) {
+		Venue venue = new Venue();
+		venue.setCategory(results.getString("category"));
+		venue.setDescription(results.getString("description"));
+		venue.setVenueID((long) results.getInt("venue_id"));
+		venue.setLocation(results.getString("location_id")); // pulls in city name and state of email
+		venue.setName(results.getString("name"));
+
+		return venue;
+	}
 
 }
